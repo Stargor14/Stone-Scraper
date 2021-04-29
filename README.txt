775b118599af8497eae2e54ba8e4b724e8f86bcc
Eryk Halicki <35389308+Stargor14@users.noreply.github.com>
2021-04-29 09:29:27 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,21 @@
+Lol another Dr. Stone inspired project
+
+In the anime, the main character uses a program he wrote to find the geographic origin of a # on twitter, since I'm a filthy copycat, I'm going to attempt to do the same thing. This project might end up turning into an ML project where I try to use a Neural Network to capture sentiment of tweets using their text, but we wil see.
+
+Give a #hashtag or a keyword to look at, will find the origin post/location of that topic
+
+Scraps twitter, looks at post hashtag and keywords to create a statistical probability that the trend started there
+
+Turns out the majority of the Twitter APIs functionality is actually locked behind a paywall! Isn't capitalism great? So instead of the Twitter API, I'm going to use selenium and beautifulsoup to scrape for information.
+Quite frankly I think that I'll learn more through this approach, so It's not necessarily a bad thing.
+
+Now that I think of it, I might as well analyze sentiment about Dr. Stone as a first query.
+
+Well I guess this is jus way too fun, I'm making a type racer bot now
+
+https://twitter.com/90spics_ <-- scrap thjis cool anime page and auto set wallpaper or something every day with random photo from their feed
+
+Next I'm going to make a binary tree visualizer that takes in multidimensional arrays and creates a png for them, or console text
+Where to run recommender, input time distance speed direction etc of where uw ant to go
+
+Turns out u cant actually find geotags of everything, especilly not through scraping, cus it is borderline impossible to seperate every element
--- scrap.py
+++ scrap.py
@@ -1,12 +1,113 @@
 import requests
 import os
 import json
-import urllib.parse
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-def run(query='cat'):
+chrome_options = Options()
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(options=chrome_options)
+
+def get_tweets(query='',scrolls=5):
     url = f'https://twitter.com/search?q={query}'
-    page = requests.get(url)
-    for i in range(len(page.text)):
-        if page.text[i:i+3] == 'wow':
-            print(page.text[i:i+6])
-run()
+    browser.get(url)
+    time.sleep(1)
+
+    body = browser.find_element_by_tag_name('body')
+    for i in range(scrolls):
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
+    tweets = browser.find_elements_by_tag_name("article")
+    return tweets
+
+def find_location(account):
+    #https://twitter.com/TheAnimeEra
+    url = f'https://twitter.com/{account}'
+    browser.get(url)
+    time.sleep(1)
+    bio = browser.find_elements_by_tag_name("span")
+    index=0
+    for i in bio:
+        try:
+            if index == 22:
+                for j in i.text:
+                    if j.isdigit():
+                        raise
+                print([i.text,index])
+            index+=1
+        except:
+            pass
+    return bio
+
+def run(tweets):
+    results = []
+    saved = []
+    for x in range(len(tweets)):
+        if x:
+            try:
+                tweet = {'msg':'','likes':0,'retweets':0,'comments':0,'type':None,'account':'','location':''}
+                data = tweets[x].find_elements_by_tag_name('span')
+                links = tweets[x].find_elements_by_tag_name('a')
+                for i in links:
+                    try:
+                        if len(i.text) >= 2:
+                            for j in range(len(i.text)):
+                                if i.text[j] == '@':
+                                    tweet['account'] = i.text[j+1:]
+                    except Exception as e:
+                        pass
+                temp = 0
+                n=0
+                going = False
+                msg=''
+                for i in data:
+                    txt = i.text
+                    if txt==temp:
+                        continue
+                    temp = txt
+                    try:
+                        if 'K' in txt:
+                            txt = float(txt[:-1])*1000
+                        txt = float(txt)
+                        if going:
+                            tweet['msg'] = msg
+                        if n==0:
+                            tweet['comments'] = int(txt)
+                        if n==1:
+                            tweet['retweets'] = int(txt)
+                        if n==2:
+                            tweet['likes'] = int(txt)
+                        going = False
+                        n+=1
+                    except Exception as e:
+                        if txt == '·':
+                            going = True
+                            continue
+                        if going:
+                            msg+=txt
+                results.append(tweet)
+                #create table of index meanings
+            except Exception as e:
+                continue
+    return results
+    #welp selenium it is
+results = []
+for i in range(5,35,5):
+    tweets = get_tweets(query='Anime',scrolls=i)
+    for j in run(tweets):
+        results.append(j)
+
+for result in results:
+    find_location(result['account'])
+    for i in range(len(result['msg'])):
+        try:
+            if result['msg'][i:i+6] == '      ':
+                result['type'] = 'meme'
+        except Exception as e:
+            #print(e)
+            pass
+for i in results:
+    print(i)
+print(len(results))
